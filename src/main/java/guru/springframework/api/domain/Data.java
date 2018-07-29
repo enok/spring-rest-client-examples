adbf3b5ad2878b452298122795d06fff7252595a
Enok <enok.jesus@gmail.com>
2018-07-29 18:50:28 -0300

    domain

---
+++ src/main/java/guru/springframework/api/domain/Data.java
@@ -0,0 +1,32 @@
+
+package guru.springframework.api.domain;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Data implements Serializable
+{
+
+    private List<User> data = null;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 3375237606774725124L;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
